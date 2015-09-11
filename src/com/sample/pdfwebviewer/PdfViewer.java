72581504bbbb739f4ba3b0b445f40e7435e3ac3c
heronmds <heron@mdstecnologia.com.br>
2015-09-11 13:35:25 -0300

    added correct behavior for all version of Android around  settings.setAllowUniversalAccessFromFileURLs(true);

--- src/com/sample/pdfwebviewer/PdfViewer.java
+++ src/com/sample/pdfwebviewer/PdfViewer.java
@@ -1,5 +1,6 @@
 package com.sample.pdfwebviewer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
@@ -11,6 +12,7 @@ public class PdfViewer extends Activity{
 	
 
 //	inside this goes our pdf viewer, just a toy for this test. Requires  more work to make it production ready
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,9 +21,12 @@ public class PdfViewer extends Activity{
 		webView = (WebView) findViewById(R.id.webView1);
 		WebSettings settings = webView.getSettings();
 		settings.setJavaScriptEnabled(true);
-//		crash on tablet and unnecessary?
-		settings.setAllowFileAccessFromFileURLs(true);
-		settings.setAllowUniversalAccessFromFileURLs(true);
+
+		//The default value is true for API level android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1 and below, 
+		//and false for API level android.os.Build.VERSION_CODES.JELLY_BEAN and above.
+		if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN)
+	        settings.setAllowUniversalAccessFromFileURLs(true);
+		
 		settings.setBuiltInZoomControls(true);
 		webView.setWebChromeClient(new WebChromeClient());
 		webView.loadUrl("file:///android_asset/pdfviewer/index.html");
